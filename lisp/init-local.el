417cfb65599b89e3756820765ed012ea8bc99f59
Andrew Myers <am2605@gmail.com>
2017-08-01 23:45:31 +1000

    Improved CFML mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -28,11 +28,11 @@
 
 ;; choose modes for CFML automatically
 (add-to-list 'magic-mode-alist
-             '("<cfcomponent" . cfml-mode))
+             '("<cfcomponent" . cftag-mode))
 (add-to-list 'magic-mode-alist
-             '("<!---" . cfml-mode))
+             '("<!---" . cftag-mode))
 (add-to-list 'auto-mode-alist
-             '("\\.cfm\\'" . cfml-mode))
+             '("\\.cfm\\'" . cftag-mode))
 (add-to-list 'auto-mode-alist
              '("\\.cfc\\'" . cfscript-mode))
 
@@ -70,6 +70,46 @@
 
 ;; (global-set-key (kbd "C-c i") 'my-indent-region)
 ;; (global-set-key (kbd "C-c u") 'my-unindent-region)
+
+(defun indent-region(numSpaces)
+  (progn
+                                        ; default to start and end of current line
+    (setq regionStart (line-beginning-position))
+    (setq regionEnd (line-end-position))
+
+                                        ; if there's a selection, use that instead of the current line
+    (when (use-region-p)
+      (setq regionStart (region-beginning))
+      (setq regionEnd (region-end))
+      )
+
+    (save-excursion ; restore the position afterwards
+      (goto-char regionStart) ; go to the start of region
+      (setq start (line-beginning-position)) ; save the start of the line
+      (goto-char regionEnd) ; go to the end of region
+      (setq end (line-end-position)) ; save the end of the line
+
+      (indent-rigidly start end numSpaces) ; indent between start and end
+      (setq deactivate-mark nil) ; restore the selected region
+      )
+    )
+  )
+
+(defun untab-region (N)
+  (interactive "p")
+  (indent-region -4)
+  )
+
+(defun tab-region (N)
+  (interactive "p")
+  (if (use-region-p)
+      (indent-region 4) ; region was selected, call indent-region
+    (insert "    ") ; else insert four spaces as expected
+    )
+  )
+
+(global-set-key (kbd "<backtab>") 'untab-region)
+;; (global-set-key (kbd "<tab>") 'tab-region)
 
 (when *is-windows*
   (setq projectile-indexing-method 'alien)
