006f9981dc3af3791b72f5cac09b03111b020bdd
Andrew Myers <am2605@gmail.com>
2017-08-08 21:39:23 +1000

    Fix cfml mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -14,19 +14,15 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")	
+	
 (setq initial-scratch-message "")
-
-(add-to-list 'load-path "~/.emacs.d/local-packages/")
-
-;;(require 'mmm-auto)
-;;(setq mmm-global-mode 'auto)
 
 (menu-bar-mode -1)
 
 (require 'mmm-mode)
-(require 'mmm-cfml)
+(require 'cfml-mode)
 
-;; choose modes for CFML automatically
 (add-to-list 'magic-mode-alist
              '("<cfcomponent" . cftag-mode))
 (add-to-list 'magic-mode-alist
@@ -34,86 +30,28 @@
 (add-to-list 'auto-mode-alist
              '("\\.cfm\\'" . cftag-mode))
 (add-to-list 'auto-mode-alist
-             '("\\.cfc\\'" . cfscript-mode))
+             '("\\.cfc\\'" . cfml-cfscript-mode))
 
-;; Use mmm-mode for highlighting of cfscript blocks in cfml files
 (setq mmm-global-mode 'maybe)
-(mmm-add-mode-ext-class nil "\\.cfm\\'" 'html-cfm)
-(mmm-add-mode-ext-class nil "\\.cfc\\'" 'html-cfm)
+(mmm-add-mode-ext-class nil "\\.cfm\\'" 'cfml-cftag)
+(mmm-add-mode-ext-class nil "\\.cfc\\'" 'cfml-cftag)
 
 (setq mmm-submode-decoration-level 0)
 
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-
+;; Indenting
 (require 'js)
-;; use spaces instead of tabs
 (setq js-indent-level 4)
-;; (setq indent-tabs-mode nil)
 (setq js-auto-indent-flag 1)
 (setq sgml-basic-offset 4)
+(setq tab-width 4)
 
-;; Set C-c i and C-c u to indent and outdent the selected region by 4 chars
-;; (defun my-indent-region (N)
-
-;;   (interactive "p")
-;;   (if (use-region-p)
-;;       (progn (indent-rigidly (region-beginning) (region-end) (* N 4))
-;;              (deactivate-mark))
-;;     (self-insert-command N)))
-
-;; (defun my-unindent-region (N)
-;;   (interactive "p")
-;;   (if (use-region-p)
-;;       (progn (indent-rigidly (region-beginning) (region-end) (* N -4))
-;;              (deactivate-mark))
-;;     (self-insert-command N)))
-
-;; (global-set-key (kbd "C-c i") 'my-indent-region)
-;; (global-set-key (kbd "C-c u") 'my-unindent-region)
-
-(defun indent-region(numSpaces)
-  (progn
-                                        ; default to start and end of current line
-    (setq regionStart (line-beginning-position))
-    (setq regionEnd (line-end-position))
-
-                                        ; if there's a selection, use that instead of the current line
-    (when (use-region-p)
-      (setq regionStart (region-beginning))
-      (setq regionEnd (region-end))
-      )
-
-    (save-excursion ; restore the position afterwards
-      (goto-char regionStart) ; go to the start of region
-      (setq start (line-beginning-position)) ; save the start of the line
-      (goto-char regionEnd) ; go to the end of region
-      (setq end (line-end-position)) ; save the end of the line
-
-      (indent-rigidly start end numSpaces) ; indent between start and end
-      (setq deactivate-mark nil) ; restore the selected region
-      )
-    )
-  )
-
-(defun untab-region (N)
-  (interactive "p")
-  (indent-region -4)
-  )
-
-(defun tab-region (N)
-  (interactive "p")
-  (if (use-region-p)
-      (indent-region 4) ; region was selected, call indent-region
-    (insert "    ") ; else insert four spaces as expected
-    )
-  )
-
-(global-set-key (kbd "<backtab>") 'untab-region)
-;; (global-set-key (kbd "<tab>") 'tab-region)
+;;(setq-default indent-tabs-mode nil)
+;;(setq c-basic-indent 4)
 
 (when *is-windows*
   (setq projectile-indexing-method 'alien)
-  (set-face-attribute 'default nil :font "Source Code Pro 10"))
+  (set-face-attribute 'default nil :font "Source Code Pro 10")
+  (setq-default cursor-type 'bar))
 (when *is-a-mac*
   (set-default-font "Monaco 14"))
 
@@ -127,6 +65,7 @@
       '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
 
+;; Theming
 (setq calendar-location-name "Tamworth, NSW, Australia")
 (setq calendar-latitude -31.1)
 (setq calendar-longitude 150.93)
@@ -143,6 +82,9 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+(require 'ivy)
+(define-key ivy-minibuffer-map (kbd "<up>") #'ivy-previous-line)
 
 (provide 'init-local)
 
